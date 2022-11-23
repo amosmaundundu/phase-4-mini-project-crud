76a6bc9d07d61a7a8d5089fa3d044067c9f85619
amosmaundundu <amosmaundu5@gmail.com>
2022-11-24 01:26:17 +0300

    done

---
+++ app/controllers/spices_controller.rb
@@ -0,0 +1,32 @@
+class SpicesController < ApplicationController
+    #GET
+    def index
+        render json: Spice.all
+    end
+
+    #POST
+    def create
+        spice = Spice.create(spice_params)
+        render json: spice, status: :created
+    end
+
+    #PATCH
+    def update
+        spice = Spice.find(params[:id])
+        spice.update(spice_params)
+        render json: spice
+    end
+
+    #DELETE
+    def destroy
+        spice = Spice.find(params[:id])
+        spice.destroy
+        head :no_content
+    end
+
+    private
+
+    def spice_params
+        params.permit(:title, :image, :description, :notes, :rating)
+    end
+end
